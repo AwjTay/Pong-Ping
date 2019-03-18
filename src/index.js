22e83be64b7f1a7be11ead8291e32bdf24e19ce7
andy <awtaylor86@gmail.com>
2019-03-18 13:04:59 +0000

    initial commit of ping-pong challenge after set-up

--- src/index.js
+++ src/index.js
@@ -2,11 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from "redux";
+
+
+const initial = {
+    player1: 0,
+    player2: 0,
+};
+
+const reducer = (state, action) => {
+	switch (action.type) {
+
+	}
+}
+
+const store = createStore(reducer, initial);
+
+store.subscribe(() => {
+
+	let state = store.getState();
+	
+
+})
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
