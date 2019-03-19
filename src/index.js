7b8579cfd01851fa819e1444f7f635a6101d8b44
andy <awtaylor86@gmail.com>
2019-03-19 10:25:23 +0000

    moving on to breaking app into components

--- src/index.js
+++ src/index.js
@@ -17,14 +17,16 @@ const setScoreP2 = state => ({...state, player2: state.player2 + 1});
 
 const setServer = state => (state.player1 + state.player2) % 5 === 0 ? {...state, serving : !state.serving } : state;
 
-const winning1 = state => state.player1 % 21 === 0 && state.player1 > (state.player2 + 1)  ? {...state, whoWins : 1 } : state;
+const serviceOver20 = state => state.player1 >= 20 && state.player2 >= 20 && (state.player1 + state.player2) % 2 === 0 ? {...state, serving : !state.serving } : state;
 
-const winning2 = state => state.player2 % 21 === 0 && state.player2 > (state.player1 + 1) ? {...state, whoWins : 2 } : state;  
+const winning1 = state => state.player1 >= 21 && (state.player1 - state.player2) > 1 ? {...state, whoWins : 1 } : state;
+
+const winning2 = state => state.player2 >= 21 && (state.player2 - state.player1) > 1 ? {...state, whoWins : 2 } : state;  
 
 const reducer = (state, action) => {
 	switch (action.type) {
-		case "incrementP1" : return winning1(setServer(setScoreP1(state)));
-		case "incrementP2" : return winning2(setServer(setScoreP2(state)));
+		case "incrementP1" : return serviceOver20(winning1(setServer(setScoreP1(state))));
+		case "incrementP2" : return serviceOver20(winning2(setServer(setScoreP2(state))));
 		case "reset" : return initial;
 		default: return state;
 	}
@@ -69,3 +71,14 @@ render();
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 
+/*const setServer = state => {
+
+	if (state.player1 >= 20 && state.player2 >= 20) {
+
+		(state.player1 + state.player2) % 2 === 0 ? {...state, serving : !state.serving } : state;
+		
+	} else {
+	
+		(state.player1 + state.player2) % 5 === 0 ? {...state, serving : !state.serving } : state;
+	}
+}*/
