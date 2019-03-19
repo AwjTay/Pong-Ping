603ec0b33b336d177a008569df1594bd200ff0f1
andy <awtaylor86@gmail.com>
2019-03-19 09:45:25 +0000

    ping-pong app complete up to 'alternating serve every two serves over 21'

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-const App = () => (
+const App = ({ player1, player2, handleIncrementP1, handleIncrementP2, handleReset, whoIsServing, whoWins }) => (
     <React.Fragment>
         {/* header */}
         <header className="page-header">
@@ -11,25 +11,25 @@ const App = () => (
         {/* scores */}
         <div className="row">
             <div className="col-xs-6">
-                <p>Player 1 <span className="pull-right label label-success">Serving</span></p>
-                <p className="well">{/* player 1 score */}</p>
-                <button className="btn btn-primary">+</button>
+                <p>Player 1 { !whoIsServing ? <span className="pull-right label label-success">Serving</span> : null} </p>
+                <p className="well">{ player1 }</p>
+                {!whoWins && <button onClick={ handleIncrementP1 } className="btn btn-primary">+</button>}
             </div>
 
             <div className="col-xs-6">
-                <p>Player 2 <span className="pull-right label label-success">Serving</span></p>
-                <p className="well">{/* player 2 score */}</p>
-                <button className="btn btn-primary">+</button>
+                <p>Player 2 { whoIsServing ? <span className="pull-right label label-success">Serving</span> : null}</p>
+                <p className="well">{ player2 }</p>
+                {!whoWins && <button onClick={ handleIncrementP2 } className="btn btn-primary">+</button>}
             </div>
         </div>
 
         { /* winner message */}
-        <h2 className="jumbotron">Player {/* winning player here */} wins!</h2>
+        <h2 className="jumbotron">{ whoWins === 0 ? "" : whoWins === 1 ? "Player One Wins" : "Player Two Wins" }</h2>
 
         <hr />
 
         { /* reset button */}
-        <button className="btn btn-danger">Reset</button>
+        <button onClick={ handleReset } className="btn btn-danger">Reset</button>
     </React.Fragment>
 );
 
--- src/index.js
+++ src/index.js
@@ -8,24 +8,58 @@ import { createStore } from "redux";
 const initial = {
     player1: 0,
     player2: 0,
+    serving : false,
+    whoWins : 0,
 };
+
+const setScoreP1 = state => ({...state, player1: state.player1 + 1});
+const setScoreP2 = state => ({...state, player2: state.player2 + 1});
+
+const setServer = state => (state.player1 + state.player2) % 5 === 0 ? {...state, serving : !state.serving } : state;
+
+const winning1 = state => state.player1 % 21 === 0 && state.player1 > (state.player2 + 1)  ? {...state, whoWins : 1 } : state;
+
+const winning2 = state => state.player2 % 21 === 0 && state.player2 > (state.player1 + 1) ? {...state, whoWins : 2 } : state;  
 
 const reducer = (state, action) => {
 	switch (action.type) {
-
+		case "incrementP1" : return winning1(setServer(setScoreP1(state)));
+		case "incrementP2" : return winning2(setServer(setScoreP2(state)));
+		case "reset" : return initial;
+		default: return state;
 	}
 }
 
-const store = createStore(reducer, initial);
 
-store.subscribe(() => {
 
-	let state = store.getState();
-	
+const store = createStore(
+	reducer, 
+	initial,
+	window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
-})
+const render = () => {
+		let state = store.getState();
+		
+		ReactDOM.render(
+			<App 
+			player1={ state.player1 } 
+			player2={ state.player2 }
+			handleIncrementP1={ () => store.dispatch({ type : "incrementP1"}) }
+			handleIncrementP2={ () => store.dispatch({ type : "incrementP2"}) }
+			handleReset={ () => store.dispatch({ type : "reset"}) }
+			whoIsServing={ state.serving }
+			whoWins={ state.whoWins }
+			/>,
+			document.getElementById("root")
+		);
+	};
 
-ReactDOM.render(<App />, document.getElementById('root'));
+store.subscribe(render);
+render();
+
+
+
 
 
 
